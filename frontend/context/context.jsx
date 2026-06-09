2360e08c370afdb29a4489b4df7c100be5a66622
HariBalaji24 <sharibalaji2006@gmail.com>
2026-06-09 13:56:48 +0530

    backend url changed

--- frontend/context/context.jsx
+++ frontend/context/context.jsx
@@ -5,7 +5,7 @@ export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
   const token = localStorage.getItem("auth-token");
-  const url = "https://fitnow-website-1.onrender.com"
+  const url = "https://fitnow-e69e.onrender.com"
   const [id, setId] = useState(null);
   const [details, setDetails] = useState(null);
   const [workoutgenerated, setworkoutgenerated] = useState();
